5a6424c0be40007bd575e9efbddd2d9a044e6020
Heidi Enbuska <henbuska@student.hive.fi>
2025-08-26 17:28:26 +0300

    Add comment for removing mock worker setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/globals.css";
 
-// Starts the service worker in dev mode -- for testing purposes
+// Starts the service worker in dev mode -- for testing purposes -> comment out if not needed!
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({
