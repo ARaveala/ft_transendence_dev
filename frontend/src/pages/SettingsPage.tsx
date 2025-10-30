5a6a3faca6772e62bc8cac07d14ee800875b2ca5
sparklebutt <arandomsanti@gmail.com>
2025-10-30 21:05:19 +0200

    delete profile now works , its a little aggresive

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -466,7 +466,8 @@ const SettingsPage: React.FC = () => {
 		try {
 			const res = await fetch(API_PROTOCOL.DELETE_PROFILE.path, {
 				method: API_PROTOCOL.DELETE_PROFILE.method,
-				headers: { "Content-Type": "application/json" },
+				//headers: { "Content-Type": "application/json" },
+				credentials: "include",
 			});
 			if (!res.ok) throw new Error("Failed to delete profile.");
 
