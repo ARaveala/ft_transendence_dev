2f631a22e7bbb706eec5f3c68a8b792ea1697be5
Anttoni Maula <amaula@c1r6p3.hive.fi>
2025-11-05 13:04:24 +0200

    More tests. Added a util function to randomize multiple users at once.

---
+++ tester/e2e/tournament1.spec.js
@@ -0,0 +1,176 @@
+import { test, expect } from '@playwright/test';
+const { randomUsername, randomizeNUsernames } = require('./utils');
+
+test('test', async ({ page }) => {
+    const usernames = randomizeNUsernames(4);
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[0]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Exit' }).click();
+  await expect(page.getByRole('heading', { name: 'Logout' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Exit' }).click();
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[1]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByText('HomeGameTournamentLeaderboardFriendsProfileSettingsExit').click();
+  await page.getByRole('link', { name: 'Exit' }).click();
+  await expect(page.getByRole('heading', { name: 'Logout' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Exit' }).click();
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[2]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Exit' }).click();
+  await expect(page.getByRole('heading', { name: 'Logout' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Exit' }).click();
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[3]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Tournament' }).click();
+  await expect(page.getByRole('heading', { name: 'Tournament' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Start a new tournament' }).click();
+  await expect(page.getByRole('textbox', { name: 'Username' }).first()).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Alias' }).first().click();
+  await page.getByRole('textbox', { name: 'Alias' }).first().fill('alias4');
+  await page.getByRole('button', { name: 'Set Alias' }).click();
+  await expect(page.getByRole('button', { name: 'Edit Alias' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).nth(1).click();
+  await page.getByRole('textbox', { name: 'Username' }).nth(1).fill(usernames[2]);
+  await page.getByRole('textbox', { name: 'Username' }).nth(1).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).nth(1).fill('asdasdasd');
+  await page.getByRole('textbox', { name: 'Password' }).nth(1).press('Tab');
+  await page.getByRole('textbox', { name: 'Alias' }).nth(1).fill('alias3');
+  await page.getByRole('button', { name: 'Add Player' }).first().click();
+  await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).nth(2).click();
+  await page.getByRole('textbox', { name: 'Username' }).nth(2).fill(usernames[1]);
+  await page.getByRole('textbox', { name: 'Username' }).nth(2).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).nth(2).fill('asdasdasd');
+  await page.getByRole('textbox', { name: 'Password' }).nth(2).press('Tab');
+  await page.getByRole('textbox', { name: 'Alias' }).nth(2).fill('alias2');
+  await page.getByRole('button', { name: 'Add Player' }).first().click();
+  await page.getByRole('textbox', { name: 'Username' }).nth(3).click();
+  await page.getByRole('textbox', { name: 'Username' }).nth(3).fill(usernames[0]);
+  await page.getByRole('textbox', { name: 'Username' }).nth(3).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).nth(3).fill('asdasdasd');
+  await page.getByRole('textbox', { name: 'Password' }).nth(3).press('Tab');
+  await page.getByRole('textbox', { name: 'Alias' }).nth(3).fill('alias1');
+  await page.getByRole('button', { name: 'Add Player' }).click();
+  await page.getByRole('button', { name: 'Start Tournament' }).click();
+  await expect(page.getByRole('heading', { name: 'Winner' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Play Match 1' }).click();
+  await expect(page.getByRole('heading', { name: 'Game Settings' })).toBeVisible();
+
+  await page.getByRole('spinbutton').click();
+  await page.getByRole('spinbutton').fill('51');
+  await page.getByRole('spinbutton').press('ArrowLeft');
+  await page.getByRole('spinbutton').fill('1');
+  await page.getByRole('button', { name: 'Confirm' }).click();
+  await page.getByRole('button', { name: 'Start Game' }).click();
+  await page.locator('iframe').contentFrame().getByText('OK').click();
+  await expect(page.getByRole('heading', { name: 'Tournament' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Play Match 2' }).click();
+  await page.locator('iframe').contentFrame().getByText('OK').click();
+  await page.getByRole('button', { name: 'Play Final' }).click();
+  await page.locator('iframe').contentFrame().getByText('OK').click();
+  await expect(page.getByRole('main')).toContainText('🏆 alias2');
+  await page.getByRole('link', { name: 'Settings' }).click();
+  await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Delete profile' }).click();
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[2]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Settings' }).click();
+  await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Delete profile' }).click();
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[1]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Settings' }).click();
+  await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Delete profile' }).click();
+  await expect(page.getByRole('button', { name: 'English' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(usernames[0]);
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('asdasdasd');
+  await page.locator('form').getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Settings' }).click();
+  await expect(page.getByRole('heading', { name: 'Settings' })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Delete profile' }).click();
+  await expect(page.getByRole('heading', { name: 'Pong' })).toBeVisible();
+});
--- tester/tests/basic.spec.ts
+++
@@ -1,6 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test('has title', async ({ page }) => {
-	await page.goto('localhost:5173');
-	await expect(page).toHaveTitle(/Pong/);
-});
